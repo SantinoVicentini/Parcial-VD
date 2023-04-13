c3c574516bb892ba137484f1e0af654b0f06a34d
AgustinFortunato <agusfortunato10@gmail.com>
2023-04-13 15:53:18 -0300

    graf 1 raw

--- vd_s2_Parcial_Fortunato_Vicentini/dataviz_1/script.js
+++ vd_s2_Parcial_Fortunato_Vicentini/dataviz_1/script.js
@@ -1,40 +1,61 @@
-d3.csv('astronautas.csv', d3.autoType).then(data => {
-    let eva_mision_hs_filtrado = data.filter(astronauta => astronauta.eva_mision_hs >= 15);
-    // Guardamos el svg generado en la variable chart
-    
-    let chart = Plot.plot({
-      marks: [
-        Plot.barX(eva_mision_hs_filtrado, {
-          x: 'eva_mision_hs',
-          y: 'nombre',
-          fill: '#333333',
-          fillOpacity: 0.5,
-          sort: { y: "x" , reverse: true},
-          
-        }),
-        
-      ],
-      style: {
-        fontFamily:"arial",
-        fontSize:12,
-      },
-      x: {
-        grid: true,
-        label: 'Mision Evacuacion Hs (→) ',
-        labelOffset: 30
-      },
-      y: {
-        label: "Nombre (→) ",
-        labelOffset: 180
-      },
-      height: 500,
-      width: 900,
-      marginLeft: 200,
-      marginTop: 40,
-      marginBottom: 40,
-    })
-    // Agregamos chart al div#chart de index.html
-    d3.select('#chart').append(() => chart)
+const mapaFetch = d3.json('barrios-caba.geojson')
+const dataFetch = d3.dsv(';', 'ruidos_molestos.csv', d3.autoType)
+
+Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
+  
+  /* Agrupamos reclamos x barrio */
+  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+  console.log('reclamosPorBarrio', reclamosPorBarrio)
+  
+  /* A cada feature del mapa le agregamos la prop DENUNCIAS */
+  barrios.features.forEach(d => {
+    let nombreBarrio = d.properties.BARRIO
+    let cantReclamos =  reclamosPorBarrio.get(nombreBarrio).length
+    d.properties.DENUNCIAS = cantReclamos
+
+    console.log(nombreBarrio + ': ' + cantReclamos)
   })
+
+
+  /* Mapa Coroplético */
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
+    },
+    color: {
+      // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+      type: 'quantize', 
+      n: 10,
+      scheme: 'ylorbr',
+      label: 'Cantidad de denuncias',
+      legend: true,
+    },
+    marks: [
+      Plot.geo(barrios, {
+        fill: d => d.properties.DENUNCIAS,
+        stroke: 'gray',
+        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
+      }),
+      Plot.text(
+        barrios.features,
+        Plot.centroid({
+          text: (d) => d.properties.BARRIO,
+          fill: "currentColor",
+          stroke: "white",
+          textAnchor: "center",
+          dx: 4,
+          filter: (d) => d.properties.DENUNCIAS > 80
+        })
+      )
+    ],
+  })
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#chart').append(() => chartMap)
+
+
+})
 
 /* ↑ → */
